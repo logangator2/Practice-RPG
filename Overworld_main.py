24adb54ae277016428b2fee610b5d0219f08bd48
logangator2 <32340228+logangator2@users.noreply.github.com>
2018-11-01 15:12:21 -0700

    Documentation fixes

--- Overworld_main.py
+++ Overworld_main.py
@@ -5,6 +5,10 @@ import Character
 import Events
 
 def main():
+    """
+    Main game loop of the Practice RPG
+    """
+    
     # main loop checker
     check = True
 
@@ -18,11 +22,14 @@ def main():
 
     event_counter = 0
 
+    # main game loop
     while (check):
+        
         # check if enough events have occurred
         if event_counter == 10:
             check = False
             break
+            
         # check if players are still alive
         all_dead = True
         for p in player_team:
@@ -32,22 +39,26 @@ def main():
             print("\nYour adventure is over.\n")
             check = False
             break
+            
         # continue adventure
         else:
             print("\nYou are walking along a road.\n")
             # ask for player input
-            command = input("What would you like to do? Type 'Help' for commands. ")
+            command = (input("What would you like to do? Type 'Help' for commands. ")
             command = command.lower()
+                       
             # display possible commands
             if (command == "help") or (command == "h"):
                 print("\nYou may enter in any of these commands: move, quit")
                 print("You may use the first letter of each command instead.")
+                       
             # trigger event
             elif (command == "move") or (command == "m"):
                 print("\nYou move along the road.")
                 #Events.event_picker(player_team)
                 Events.event_picker(player_team)
                 event_counter += 1
+                       
             # quit game
             elif(command == "quit") or (command == "q"):
                 check = False
